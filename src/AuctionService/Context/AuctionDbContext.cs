59e80d171df09992d1b7560eedd001039f3b1629
Kamil Studzinski <studnia12321@yahoo.com>
2024-03-17 20:23:40 +0100

    + Models and Migrations

---
+++ src/AuctionService/Context/AuctionDbContext.cs
@@ -0,0 +1,10 @@
+﻿using AuctionService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Context
+{
+    public class AuctionDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Auction> Auctions { get; set; }
+    }
+}
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,8 +1,15 @@
+using AuctionService.Context;
+using Microsoft.EntityFrameworkCore;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 WebApplication app = builder.Build();
 
