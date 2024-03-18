40af7196c79cb0b4a81b6e2212edc5e74359dc66
Kamil Studzinski <studnia12321@yahoo.com>
2024-03-18 09:31:08 +0100

    + DbQuerying

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.DTOs;
+using SearchService.Models;
+
+namespace SearchService.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
+        {
+            PagedSearch<Item> query = DB.PagedSearch<Item>();
+
+            query.Sort(x => x.Ascending(a => a.Make));
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                query.Match(Search.Full, searchTerm).SortByTextScore();
+            }
+
+            query.PageNumber(pageNumber);
+            query.PageSize(pageSize);
+
+            (IReadOnlyList<Item> Results, long TotalCount, int PageCount) result = await query.ExecuteAsync();
+
+            return Ok(new
+            {
+                results=result.Results,
+                pageCount=result.PageCount,
+                totalCount=result.TotalCount,
+            });
+        }
+    }
+}
