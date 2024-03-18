c73806dbbcf00c5d1e06f6b3c0806f1b7abbee39
Kamil Studzinski <studnia12321@yahoo.com>
2024-03-18 08:27:25 +0100

    + Search Service Source Files

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,10 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+WebApplication app = builder.Build();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
