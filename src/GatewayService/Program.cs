be3de94bdccd1ba383639d7d1aee490649284652
Kamil Studzinski <studnia12321@yahoo.com>
2024-03-18 22:39:09 +0100

    + Gateway Service Source File

---
+++ src/GatewayService/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
+
+WebApplication app = builder.Build();
+
+app.MapReverseProxy();
+
+app.Run();
