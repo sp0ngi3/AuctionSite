aac5c12d48391b26d15e946d340d329932faac1d
Kamil Studzinski <studnia12321@yahoo.com>
2024-03-25 20:25:28 +0100

    + Web-App Src Files

---
+++ frontend/web-app/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Qtlo9xSj1c8ujtZbVSNVZe1oIltKAymA9YiR3j+UkA4=\"\n}"
---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h3 className="text-3xl font-semibold">Auction Site</h3>
+    </div>
+  );
+}
